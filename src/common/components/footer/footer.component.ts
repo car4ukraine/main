d64a22a30159f0f254594a95e5f500eadc29b6f0
Oleksii Dovhaniuk <alexdovhaniuk@gmail.com>
2024-06-02 18:37:53 +0300

    2024-06-02 Init footer

--- src/common/components/footer/footer.component.ts
+++ src/common/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AppNavigationComponent } from "../app-navigation/app-navigation.component";
 
 @Component({
-  selector: 'app-footer',
-  standalone: true,
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+    selector: 'app-footer',
+    standalone: true,
+    templateUrl: './footer.component.html',
+    styleUrl: './footer.component.scss',
+    imports: [AppNavigationComponent]
 })
 export class FooterComponent {
 
