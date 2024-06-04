5fc95f5ae8b82505fc1980cebca5456acc8fc748
Oleksii Dovhaniuk <alexdovhaniuk@gmail.com>
2024-06-04 10:10:42 +0300

    2024-06-04 Fill stories

--- src/pages/home-page/sections/stories-section/stories-section.component.ts
+++ src/pages/home-page/sections/stories-section/stories-section.component.ts
@@ -9,4 +9,45 @@ import { Component } from '@angular/core';
 })
 export class StoriesSectionComponent {
   public readonly title: string = 'Buy Car for Us';
+  public readonly learnMoreLabel: string = 'Learn more';
+  public readonly learnMoreTitle: string = ' learn more link';
+  public readonly donateCarLabel: string = 'Donate car';
+  public readonly donateCarTitle: string = 'Redirect to donate car page';
+  public readonly assetsPath: string = '/assets/images/home/stories/';
+  public readonly stories: Array<{
+    title: string,
+    details: string,
+    href: string,
+    iframe: string,
+    image: string
+  }> = [
+    {
+      title: 'Adam from USA',
+      details: 'Adam always wanted to help Ukraine with a tangible donation. In February 2023, he came to Donbas and personally donated two trucks to soldiers.',
+      href: 'https://www.youtube.com/watch?v=z8pCibdfCkY',
+      iframe: '',
+      image: '1-adam.jpg'
+    },
+    {
+      title: 'Martin Buhr - Vacation in Ukraine',
+      details: 'Excited to help Ukraine firsthand, Martin spent his September 2023 vacation bringing a truck to aid in demining efforts.',
+      href: 'https://tallmartin.substack.com/p/coda-my-truck-heads-to-the-russo?utm_source=profile&utm_medium=reader2',
+      iframe: '',
+      image: '2-martin-buhr.jpg'
+    },
+    {
+      title: 'British Business Bought us Truck',
+      details: 'UK\'s SUV Prestige dealership purchased an L200 pickup, organized a volunteer convoy to Lviv, and toured shops and the city, creating a compelling story.',
+      href: 'https://www.youtube.com/watch?v=G6rE1wfWpT8',
+      iframe: '',
+      image: '3-british-business.jpg'
+    },
+    {
+      title: 'Edward and Hugh from South Africa',
+      details: 'Born in South Africa and raised in a British-Canadian community, Edward and Hugh personally brought a pickup truck that now defends Ukraine in the East.',
+      href: 'https://twitter.com/carforukraine/status/1743748880808779951',
+      iframe: '',
+      image: '4-edward-and-hugh.jpg'
+    }
+  ];
 }
