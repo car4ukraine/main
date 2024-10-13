7f90f052227411eb9563f47fd07cf9ae3f48970f
Karba <ivan.karbashevskyi@gmail.com>
2024-10-13 22:59:13 +0200

    Add View Transitions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,15 @@
-import {
-  ApplicationConfig,
-  provideExperimentalCheckNoChangesForDebug,
-  provideExperimentalZonelessChangeDetection,
-  provideZoneChangeDetection
-} from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideExperimentalZonelessChangeDetection} from '@angular/core';
+import {provideRouter, withViewTransitions} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import {routes} from './app.routes';
+import {provideClientHydration} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withViewTransitions()
+    ),
     provideClientHydration(),
     provideExperimentalZonelessChangeDetection(),
     // provideZoneChangeDetection({
