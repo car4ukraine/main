61d6bd8e6eed411e016ee1e64f7d053434d89406
Karba <ivan.karbashevskyi@gmail.com>
2024-10-22 18:40:33 +0200

    Fix problem with languages

--- src/pages/languages-page/languages.page.ts
+++ src/pages/languages-page/languages.page.ts
@@ -16,42 +16,52 @@ export default class LanguagesPage {
   public readonly languageList: {
     name: string;
     href: string;
+    code: string;
   }[] = [
     {
       name: 'English (US)',
-      href: '/'
+      href: '/',
+      code: 'en-US',
     },
     {
       name: 'Dansk',
       href: '/da',
+      code: 'da'
     },
     {
       name: 'Deutsch',
       href: '/de',
+      code: 'de'
     },
     {
       name: 'Español',
       href: '/es',
+      code: 'es'
     },
     {
       name: 'Français',
       href: '/fr',
+      code: 'fr'
     },
     {
       name: 'Norsk Bokmål',
       href: '/nb',
+      code: 'nb'
     },
     {
       name: 'Nederlands',
       href: '/nl',
+      code: 'nl'
     },
     {
       name: 'Polski',
       href: '/pl',
+      code: 'pl'
     },
     {
       name: 'Svenska',
       href: '/sv',
+      code: 'sv'
     }
   ];
 
