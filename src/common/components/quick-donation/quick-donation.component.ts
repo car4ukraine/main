73a94f2f560999f5d6546eef95e737a70ce47e08
chopyk89 <if040489cvv@gmail.com>
2025-01-20 17:23:38 +0200

    add translate

--- src/common/components/quick-donation/quick-donation.component.ts
+++ src/common/components/quick-donation/quick-donation.component.ts
@@ -8,7 +8,11 @@ import {ChangeDetectionStrategy, Component, ViewEncapsulation} from "@angular/co
   template: `
     <div class="text-6xl font-tektur text-[#1F2125] font-bold drop-shadow-xl" i18n>Quick Donation</div>
     <div>
-
+      <iframe src="https://donorbox.org/embed/power-the-frontline?language=en-us" name="donorbox"
+              allowpaymentrequest="allowpaymentrequest" seamless="seamless" frameborder="0" scrolling="no"
+              height="900px" width="100%" style="max-width: 500px; min-width: 250px; max-height:none!important"
+              allow="payment">
+      </iframe>
     </div>
   `,
   host: {
