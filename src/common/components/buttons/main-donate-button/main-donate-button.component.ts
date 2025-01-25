b912a95838624dfaff678f5c7f35f7d4b77d8642
Karba <ivan.karbashevskyi@gmail.com>
2025-01-25 14:19:15 +0100

    Fix btn

--- src/common/components/buttons/main-donate-button/main-donate-button.component.ts
+++ src/common/components/buttons/main-donate-button/main-donate-button.component.ts
@@ -17,7 +17,8 @@ import {bootstrapHeart} from "@ng-icons/bootstrap-icons";
   ],
   template: `
     <a target="_blank"
-       class="w-full min-w-[340px] h-[70px] px-[18px] bg-[#1f2024] justify-center items-center gap-[7px] inline-flex">
+       href="https://donorbox.org/ukraine-matters-life-is-the-greatest-value"
+       class="w-full min-w-[340px] h-[70px] px-[18px] bg-[#1f2024] justify-center items-center gap-[7px] inline-flex cursor-pointer">
       <ng-icon class="text-[#FEC639]" name="bootstrapHeart" size="1.5em"/>
       <div
         class="text-center text-[#f7f8f7] text-2xl font-medium font-tektur leading-loose tracking-[2.88px]" i18n>
--- src/pages/home-page/buttons/donate-car-button/donate-car-button.component.ts
+++ src/pages/home-page/buttons/donate-car-button/donate-car-button.component.ts
@@ -6,7 +6,7 @@ import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/co
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <a href="/donate-car" class="w-full min-w-[340px] h-[70px] px-[18px] border-2 border-[#1f2024] justify-center items-center gap-[7px] inline-flex">
+    <a href="/donate-car" class="w-full min-w-[340px] h-[70px] px-[18px] border-2 border-[#1f2024] justify-center items-center gap-[7px] inline-flex cursor-pointer">
       <div class="text-center text-[#1f2024] text-2xl font-medium font-['Tektur'] leading-loose tracking-[2.88px]">DONATE CAR</div>
     </a>
   `
