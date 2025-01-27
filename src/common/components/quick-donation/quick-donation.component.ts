a06d8574800f9dabc3e871d18916d0a2f3e955b9
Karba <ivan.karbashevskyi@gmail.com>
2025-01-27 20:40:51 +0100

    27.01.2025:1

--- src/common/components/quick-donation/quick-donation.component.ts
+++ src/common/components/quick-donation/quick-donation.component.ts
@@ -8,10 +8,8 @@ import {ChangeDetectionStrategy, Component, ViewEncapsulation} from "@angular/co
   template: `
     <div class="text-6xl font-tektur text-[#1F2125] font-bold drop-shadow-xl" i18n>Quick Donation</div>
     <div>
-      <iframe src="https://donorbox.org/embed/power-the-frontline?language=en-us" name="donorbox"
-              allowpaymentrequest="allowpaymentrequest" seamless="seamless" frameborder="0" scrolling="no"
-              height="900px" width="100%" style="max-width: 100%; min-width: 100%; max-height:none!important"
-              allow="payment">
+      <iframe src="https://donorbox.org/embed/quick-donation-c4u?designation=Upgrade%20donated%20cars&default_interval=o" name="donorbox" allowpaymentrequest="allowpaymentrequest" seamless="seamless" frameborder="0" scrolling="no" height="900px" width="100%" style="max-width: 500px; min-width: 250px; max-height:none!important" allow="payment">
+
       </iframe>
     </div>
   `,
